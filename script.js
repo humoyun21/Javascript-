3dbe1b394a3dc9f7360f26bc51ce3c798e05bf0f
humoyun21 <husanovhumoyunbek20052109@gmail.com>
2024-01-09 02:15:17 +0500

    question ansver

---
+++ script.js
@@ -0,0 +1,136 @@
+//let number = +prompt("Please enter a number");
+//if (Number.isInteger(number) && number % 2 == 0) {
+//  console.log ("Juft son");
+//}else{
+//    console.log ("toq son");
+//}
+
+/*let number = +prompt("Please enter a number");
+let number1 = +prompt("Please enter a number2");
+if (Number.isInteger(number) && Number.isInteger(number1) &&  number>0 && number1>0) {
+    console.log ("musbat");
+}else{
+    console.log ("manfiy");
+}*/
+
+/*let a = +prompt("Please enter a ");
+let b = +prompt("Please enter b ");
+let c = +prompt("Please enter c ");
+if (
+  Number.isInteger(a) &&
+  Number.isInteger(b) &&
+  Number.isInteger(c) &&
+  a > b &&
+  b > c
+) {
+  console.log("a bdan katta b cdan katta");
+} else {
+  console.log("error");
+}
+*/
+
+/*
+let a = +prompt("Please enter a ");
+let b = +prompt("Please enter b ");
+let c = +prompt("Please enter c ");
+if (a > b && b > c) {
+  console.log("a bdan katta b cdan katta");
+} else {
+  console.log("error");
+}
+*/
+
+/*
+let number = +prompt("Please enter a number");
+let number1 = +prompt("Please enter a number2");
+if (number% 2==1 && number1 % 2==1) {
+    console.log ("toq");
+}else{
+    console.log ("juft");
+}
+
+*/
+
+/*
+let a = +prompt("please enter a ");
+let b = +prompt("please enter b");
+let c = +prompt("please enter c");
+let d = +prompt("please enter d");
+if(a > 0 && b > 0 && c > 0 && d > 0 ) {
+     console.log("musbat")
+}else{
+     console.log("manfiy son")
+}*/
+
+//12
+/*
+let a = +prompt("Please enter a ");
+if (Number.isInteger(a) && a > 0) {
+  console.log(a + 1);
+} else {
+  console.log(a - 1);
+}
+*/
+
+//13
+/*
+let a = +prompt("Please enter a ");
+if (Number.isInteger(a) && a > 0) {
+  console.log(a + 3);
+} else {
+  console.log(a - 2);
+}
+*/
+
+//14
+/*
+let a = +prompt("please enter a ");
+let b = +prompt("please enter b");
+if (Number.isInteger(a) && Number.isInteger(b)) {
+  console.log(Math.max(a ,b));
+}
+*/
+
+//15
+/*
+let a = +prompt("please enter a ");
+let b = +prompt("please enter b ");
+let c = +prompt("please enter c ");
+if (Number.isInteger(a) && Number.isInteger(b) && Number.isInteger(c)) {
+  console.log(Math.max(a, b, c));
+}
+*/
+
+//16
+/*
+let a = +prompt("please enter a ");
+let b = +prompt("please enter b ");
+let c = +prompt("please enter c ");
+if (Number) {
+  console.log(Math.min(a, b, c));
+}
+*/
+/*
+var number = parseInt(prompt("Raqam kiriting:"));
+if (number > 0){
+   if (number % 2 == 1) {
+        console.log("Raqam ijobiy va toq");
+   } else {
+        console.log("Raqam ijobiy va juft");
+   }
+} else if (number < 0) {
+   if (number % 2 == -1) {
+     console.log("Raqam manfiy va toq");
+   } else {
+     console.log("Raqam manfiy va juft");
+   }
+} else {
+   console.log("Raqam nolga teng");
+}
+*/  
+
+//20
+let a = +prompt("please enter your was born ");
+if( a=2024-a){
+    console.log(" ${} sizning yoshingiz" )
+}
